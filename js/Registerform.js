d5fa745a01a5b4108f9e03a1ef2860deceee62de
Michele Sorbo <michele.sorbo@gmail.com>
2022-11-15 12:50:03 +0100

    init

---
+++ js/Registerform.js
@@ -0,0 +1,13 @@
+/*
+Username: min 4 caratteri e senza numeri e caratteri speciali
+
+Email: controllare che sia in un formato valido
+
+Password: min 8 caratteri e almeno 1 carattere maiusco, uno numerico e un carattere speciale
+
+Conrollare che la password e la password di conferma siano uguali
+
+Per il css creare una regola per visualizzare l'errore sotto la line bianca
+Se non si riesce usare alert
+
+*/
